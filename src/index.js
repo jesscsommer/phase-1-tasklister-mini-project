4164dd756415f8b74cb65fdfcb5d74ac6b4c55cb
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-04-12 13:43:09 -0700

    Adds minimum deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+const submitBtn = document.querySelector('input[type=submit]');
+
+function handleTodo(event){
+  event.preventDefault();
+  const newTodo = document.createElement('li');
+  const newTodoDescription = document.querySelector('#new-task-description').value;
+  newTodo.innerText = newTodoDescription;
+  document.querySelector('div#list > ul').append(newTodo);
+}
+
+submitBtn.addEventListener('submit', handleTodo);
