2c1be096fa5df618a16ff3eb30ef555339c1e137
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-04-13 08:41:12 -0700

    Adds ability to delete todos

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   const form = document.querySelector('#create-task-form');
 
   form.addEventListener('submit', handleTodo);
 
   function handleTodo(event){
     const newTodo = document.createElement('li');
+
     const newTodoDescription = document.querySelector('#new-task-description').value;
     newTodo.innerText = newTodoDescription;
+
+    const newDeleteBtn = document.createElement('button');
+    newDeleteBtn.innerText = 'x';
+    newDeleteBtn.className = 'deleteBtn';
+    newTodo.append(newDeleteBtn);
+
     document.querySelector('div#list > ul').append(newTodo);
     event.preventDefault();
-  }
 
+    newDeleteBtn.addEventListener('click', (event) => event.target.parentElement.remove());
+  }
 });
